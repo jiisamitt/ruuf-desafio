3ff0685cd43c0cb86a746f99c4ac8dc1d67c66cd
jiisamitt <53799239+jiisamitt@users.noreply.github.com>
2024-05-01 18:15:49 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -2,25 +2,29 @@
 # Function to calculate the number of pannels that fit in the roof
 def pannels_fitting(pannel_x, pannel_y, roof_x, roof_y):
   
-  # Calculate the number of pannels that fit in the roof
-  
   # Amount of pannels that fit in the roof, without considering the "remainder" of the roof
   r_x_with_p_x = int((roof_x/pannel_x))*int((roof_y/pannel_y))
   
-  # If the pannel from the small side fits in the rest of the roof
+  # If the pannel from one sides fits in the rest of the roof, we add it
   rest = 0
   if roof_x%pannel_x >= pannel_y:
-    rest = (int((roof_x%pannel_x)/(pannel_y)))*int(roof_y/pannel_x)
+    rest += (int((roof_x%pannel_x)/(pannel_y)))*int(roof_y/pannel_x)
+  
+  if roof_y%pannel_y >= pannel_x:
+    rest += (int((roof_y%pannel_y)/(pannel_x)))*int(roof_x/pannel_y)
   
   first_option =  r_x_with_p_x + rest
   
   # Now we do the same, but with the other side of the roof
   r_y_with_p_x = int((roof_x/pannel_y))*int((roof_y/pannel_x))
   
-  # If the pannel from the small side fits in the rest of the roof
+  # If the pannel from one sides fits in the rest of the roof, we add it
   rest = 0
   if roof_x%pannel_y >= pannel_x:
-    rest = (int((roof_x%pannel_y)/(pannel_x)))*int(roof_y/pannel_y)
+    rest += (int((roof_x%pannel_y)/(pannel_x)))*int(roof_y/pannel_y)
+  
+  if roof_y%pannel_x >= pannel_y:
+    rest += (int((roof_y%pannel_x)/(pannel_y)))*int(roof_x/pannel_x)
   
   second_option = r_y_with_p_x + rest
   
